204e0b66fa87616284e469ed84eccdd1be3a88e6
qq8721443 <qq8721443@naver.com>
2022-05-14 01:39:00 +0900

    Add postcss

--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,7 @@ module.exports = {
             },
             {
                 test: /\.s?css$/,
-                use: ['vue-style-loader', 'css-loader', 'sass-loader']
+                use: ['vue-style-loader', 'css-loader', 'postcss-loader', 'sass-loader']
             }
         ]
     },
