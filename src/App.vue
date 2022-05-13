6e95285362de583b3497a06b3128d8c9370fd994
qq8721443 <qq8721443@naver.com>
2022-05-14 01:29:54 +0900

    Add router

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,3 @@
 <template>
-    <header>
-        <RouterLink to="/">Home..</RouterLink>
-        <RouterLink to="/about">About..</RouterLink>
-    </header>
-    <section>
-        <RouterView></RouterView>
-    </section>
+    <RouterView />
 </template>
-<script>
-import Button from '~/components/Button';
-
-export default {
-    components: {
-        Button
-    },
-    data() {
-        return {
-            msg: 'Hello Vue!'
-        }
-    },
-}
-</script>
-<style lang="scss" scoped>
-header {
-    background-color: #fff;
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 80px;
-    display: flex;
-    align-items: center;
-}
-section {
-    margin-top: 80px;
-}
-</style>
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,20 +1,13 @@
-import { createRouter, createWebHistory } from 'vue-router';
-import Home from './Home';
-import About from './About';
+import { createRouter, createWebHistory } from 'vue-router'
+import Home from './Home'
 
 export default createRouter({
     history: createWebHistory(),
-    scrollBehavior() {
-        return { top: 0 }
-    },
+    scrollBehavior: () => ({ top: 0 }),
     routes: [
         {
             path: '/',
             component: Home
-        },
-        {
-            path: '/about',
-            component: About,
         }
     ]
 })
